ec8ff2703f9edc25cbf230f43bdaeb1fa5257df4
Matias Gabriel Priano <mapriano@emeal.nttdata.com>
2024-06-13 23:17:59 -0300

    Mejora en /products: cambio iconos y agrego nueva columna

--- components/productGridRow.tsx
+++ components/productGridRow.tsx
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import productsList from "./productsMock";
 
 interface ProductGridRowProps {
     name: string;
@@ -16,9 +15,13 @@ export default function ProductGridRow({ product }: {product: ProductGridRowProp
         router.push(`/products/${product.name}/${product.version}`)
     }
 
+    const handleNewTicketButton = () => {
+        router.push(`/products/${product.name}/${product.version}/new`)
+    }
+
     return (
         <tr key={`${product.name}`} className="h-10">
-            <td className="px-2 py-3 w-1/4">
+            <td className="px-2 py-3 w-2/4">
                 <div className="flex items-center">
                     <div className="text-s font-medium text-gray-900 max-w-xs">
                         <div className="truncate">{product.name}</div>
@@ -26,7 +29,7 @@ export default function ProductGridRow({ product }: {product: ProductGridRowProp
                 </div>
             </td>
 
-            <td className="px-2 py-3 w-1/4">
+            <td className="px-2 py-3 w-1/6">
                 <div className="flex items-center justify-center">
                     <div className="text-s font-medium text-gray-900 max-w-xs">
                         {product.version}
@@ -34,10 +37,25 @@ export default function ProductGridRow({ product }: {product: ProductGridRowProp
                 </div>
             </td>
 
+            <td className="px-2 py-3 w-1/6">
+                <div className="flex items-center justify-center">
+                    <button type="button" onClick={handleSeeAssociatedTickets} className="relative text-black hover:text-blue-600">
+                        <svg className="w-7 h-7 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" 
+                                height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 3v4a1 1 0 0 1-1 1H5m8 7.5 2.5 2.5M19 4v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1Zm-5 9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"/>
+                        </svg>
+                    </button>
+                </div>
+            </td>
+
             <td className="px-2 py-3 w-1/4">
                 <div className="flex items-center justify-center">
-                    <input type="image" src="https://img.icons8.com/?size=100&id=4907&format=png&color=000000" className="w-7 h-7"
-                        onClick={handleSeeAssociatedTickets}></input>
+                    <button type="button" onClick={handleNewTicketButton} className="relative text-black hover:text-blue-600">
+                        <svg className="w-7 h-7" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
+                                width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h4M9 3v4a1 1 0 0 1-1 1H4m11 6v4m-2-2h4m3 0a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z"/>
+                        </svg>
+                    </button>
                 </div>
             </td>
         </tr>
--- pages/products.tsx
+++ pages/products.tsx
@@ -3,7 +3,7 @@ import ProductGridRow from "@/components/productGridRow";
 import productsList from "@/components/productsMock";
 
 function HeaderItem({ title }: { title: string }) {
-    return <th className="px-6 py-3 text-sm text-white border-b border-gray-200 bg-blue-950">{title}</th>
+    return <th className="px-6 py-2 text-sm text-white bg-blue-950">{title}</th>
 }
 
 export default function Products() {
@@ -15,19 +15,20 @@ export default function Products() {
     return (
         <>
                 {/* ACA EMPIEZA LA GRILLA */}
-                <div className="container max-w-7xl mx-auto">
+                <div className="container max-w-7xl mx-auto mt-8">
                     <div className="mb-4">
                         <h1 className="text-3xl font-bold text-gray-900 decoration-gray-400">Productos</h1>
                     </div>
                     <div className="flex flex-col">
                         <div className="overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-                            <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 bg-gray-200 shadow sm:rounded-lg">
+                            <div className='rounded-xl border border-blue-950 overflow-hidden'>
                                 <table className="min-w-full">
                                     <thead>
                                     <tr>
                                         <HeaderItem title="Nombre" />
                                         <HeaderItem title="Versión" />
-                                        <HeaderItem title="Ver tickets asociados" />
+                                        <HeaderItem title="Tickets asociados" />
+                                        <HeaderItem title="Crear ticket" />
                                     </tr>
                                     </thead>
 
