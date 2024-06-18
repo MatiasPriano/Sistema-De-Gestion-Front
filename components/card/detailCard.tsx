0f26c8e7b8cf2e1237f2fcb215a36bb1e4168c11
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2024-06-18 00:10:16 -0300

    Mejoras a pantalla de ticket

--- components/card/detailCard.tsx
+++ components/card/detailCard.tsx
@@ -5,13 +5,15 @@ interface DetailCardProps {
 }
 
 export default function DetailCard({ mainContent, title, wide = false }: DetailCardProps) {
-    const width = wide ? "w-48" : "w-32"
+    const width = wide ? "w-48" : "w-32";
+
     return (
-        <div className={`relative flex flex-col items-center justify-center rounded-xl ${width} h-32 border border-transparent hover:border-amber-500 transition duration-300 ease-in-out`}>
-            <div className="flex flex-col items-center justify-center flex-grow pb-4">
+        <div className={`overflow-hidden relative flex flex-col items-center justify-center rounded-xl ${width} h-32 border border-gray-200 transition duration-300 ease-in-out hover:border-t hover:border-r hover:border-l hover:border-blue-950`}>
+            <div className=" block flex flex-col items-center justify-center flex-grow pb-4">
                 {mainContent}
             </div>
-            <label className="text-sm absolute bottom-2">{title}</label>
+            <div className="absolute bottom-0 left-0 right-0 h-2 bg-gray-300"></div>
+            <label className="text-sm absolute bottom-3">{title}</label>
         </div>
     );
 }
--- components/ticketDetails.tsx
+++ components/ticketDetails.tsx
@@ -21,7 +21,7 @@ export default function TicketDetails({ ticket }: { ticket: Ticket }) {
     return (
         <div className="px-4 py-8 space-y-8 rounded-xl border border-blue-950 overflow-hidden">
             <div className="flex items-center">
-                <h1 className="text-4xl font-bold text-gray-900 font-sans">{ticket.title}</h1>
+                <h1 className="text-4xl font-bold text-gray-900 font-sans line-clamp-1">{ticket.title}</h1>
                 <h2 className="text-black mx-4 text-2xl text-gray-600">
                     {"#" + ticket.id}
                 </h2>
@@ -32,7 +32,7 @@ export default function TicketDetails({ ticket }: { ticket: Ticket }) {
                 <DetailCard
                     mainContent={
                         <div>
-                            <h1 className={`text-2xl font-extrabold text-black ${coloursMap[severityColours[ticket.severity]]} p-2 rounded-xl`}>
+                            <h1 className={`flex items-center justify-center text-2xl font-extrabold text-black ${coloursMap[severityColours[ticket.severity]]} w-12 h-12 rounded-2xl`}>
                                 {ticket.severity}
                             </h1>
                         </div>
@@ -47,11 +47,23 @@ export default function TicketDetails({ ticket }: { ticket: Ticket }) {
                     title="Estado" />
                 <DetailCard
                     mainContent={
-                        <h1 className="text-center text-xl font-extrabold text-gray-900 flex items-center justify-center px-4">
-                            {ticket.client}
-                        </h1>
+                        <div className="text-center text-xl font-extrabold text-white flex items-center justify-center bg-blue-950 p-2 rounded-xl mx-2">
+                            <div className="line-clamp-2 word-wrap">
+                                {ticket.client}
+                            </div>
+                        </div>
                     }
-                    title="Estado"
+                    title="Cliente"
+                    wide={true} />
+                <DetailCard
+                    mainContent={
+                        <div className="text-center text-xl font-extrabold text-gray-900 flex items-center justify-center px-4">
+                            <div className="line-clamp-3 word-wrap">
+                                {ticket.responsable}
+                            </div>
+                        </div>
+                    }
+                    title="Responsable"
                     wide={true} />
             </div>
             <hr/>
