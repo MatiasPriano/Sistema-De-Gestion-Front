c5d80a6cc29423a756d25b34266bf0d25e6357fe
GianlucaNegrotti <gianluca@DESKTOP-AAOT8C6.localdomain>
2024-06-20 00:31:06 -0300

    creo branch proyectos

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,30 +1,42 @@
 import Link from "next/link"
 import { Toaster } from "react-hot-toast";
+import { useState } from "react";
 
 export default function Layout({ children, pageTitle }: { children: any, pageTitle: any }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <nav>
-      <div className="fixed top-0 left-0 w-full bg-primary z-[9999]">
+      <div className="fixed top-0 left-0 w-full bg-blue-950 z-[9999]">
         <div className="relative flex h-14 items-center justify-between">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">    
               <Link href="/home" className="flex flex-shrink-0 items-center">
-                <h1 className="text-darkTitle font-bold text-4xl px-2 h-11">PSA</h1>
-                <h1 className="text-darkTitle font-semibold text-xl h-2 italic">{pageTitle}</h1>
+                <h1 className="text-white font-bold text-4xl px-2 h-11">PSA</h1>
+                <h1 className="text-white font-semibold text-xl h-2 italic">{pageTitle}</h1>
               </Link>   
             
 
             <div className="hidden sm:ml-8 sm:block">
               <div className="flex space-x-4">
-                <Link href="/projects" className="bg-primary hover:bg-primaryHover text-darkTitle rounded-md px-3 py-2 font-medium transition-colors duration-300 ease-in-out">Proyectos</Link>
-                <Link href="/products" className="bg-primary hover:bg-primaryHover text-darkTitle rounded-md px-3 py-2 font-medium transition-colors duration-300 ease-in-out">Soporte</Link>
+                <div className="relative group"
+                onMouseEnter={() => setIsMenuOpen(true)}
+                onMouseLeave={() => setIsMenuOpen(false)}
+                >
+                  <Link href="/projects" className="relative top-2 bg-blue-950 hover:bg-blue-900 text-white rounded-md px-3 py-2 font-medium">Proyectos</Link>
+                  <div className="fixed top-9 absolute hidden group-hover:block bg-blue-950 text-white rounded-md mt-1 w-48">
+                    <Link href="gestiondeproyectos" className="block px-4 py-2 hover:bg-blue-900">Gestion de proyectos</Link>
+                    <Link href="historialdeproyectos" className="block px-4 py-2 hover:bg-blue-900">Historial de proyectos</Link>
+                    <Link href="creaciondeproyectos" className="block px-4 py-2 hover:bg-blue-900">Creación de proyectos</Link>
+                  </div>
+                </div>
+                  <Link href="/products" className="bg-blue-950 hover:bg-blue-900 text-white rounded-md px-3 py-2 font-medium">Soporte</Link>
               </div>
             </div>
           </div>  
         </div>
       </div>
 
-      <div className="min-h-screen flex flex-col bg-background">
+      <div className="min-h-screen flex flex-col bg-white">
           <div className="p-6 pt-20">
             <main className="flex-1">
               <Toaster position="bottom-center" />
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,9 @@
+
 import {useEffect, useState} from "react";
+import productsList from "@/components/productsMock";
+import Breadcrumb from "@/components/breadcrumb";
+import ProductTable from "@/components/compactTable/products/productTable";
+
 
 function HeaderItem({ title }: { title: string }) {
     return <th className="px-6 py-3 text-sm text-left text-gray-500 border-b border-gray-200 bg-gray-50">{title}</th>
@@ -11,9 +16,20 @@ export default function Projects() {
     }, [])
 
     return (
-        <>
-                {/* ACA EMPIEZA LA GRILLA */}
-                
-        </>
+      
+             
+                <>
+                   {/* ACA EMPIEZA LA GRILLA */}
+        <Breadcrumb steps={[
+            { name: "", link: null }
+        ]} />
+        <div className="space-y-4">
+            <header className="flex items-center">
+                <h1 className="text-2xl sm:text-4xl font-bold decoration-gray-400 text-gray-900 line-clamp-2 sm:line-clamp-1">Productos</h1>
+            </header>
+            <ProductTable products={productsList} />
+        </div>
+    </>
+                 
     )
 }
