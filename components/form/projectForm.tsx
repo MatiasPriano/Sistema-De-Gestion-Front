798c797b0853de9ec3c8a83708bada31efc5285d
MatiasPriano <caimatias92.mp@gmail.com>
2024-06-22 15:47:56 -0300

    Campo fecha inicio

--- components/form/projectForm.tsx
+++ components/form/projectForm.tsx
@@ -163,7 +163,20 @@ export default function ProjectForm (
                             disabled={disabledInputs.state}
                             onChange={setState} />
                     </div>
-                        <div className='flex col-span-full pt-2'>
+                    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6">
+                        <div className="space-y-2">
+                            <label htmlFor="input" className="block text-sm font-medium text-title">Fecha de inicio</label>
+                                <div className="relative max-w-sm">
+                                    <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                        <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                        </svg>
+                                    </div>
+                                    <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seleccionar fecha"/>
+                                </div>
+                        </div>
+                    </div>
+                    <div className='flex col-span-full pt-2'>
                             <div className="flex items-center justify-end gap-x-6 px-4 w-full">
                                 <TextButton
                                     name="Cancelar"
@@ -174,7 +187,7 @@ export default function ProjectForm (
                                     type="submit"
                                     style="primary" />
                             </div>
-                        </div>
+                    </div>
                 </main>
             </form>
         )
