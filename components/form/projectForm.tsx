5922bdcc23fb6704b355d8d0986e45b017284f3d
MatiasPriano <caimatias92.mp@gmail.com>
2024-06-24 13:07:02 -0300

    Bloqueo de campos fecha en ver Proyecto

--- components/form/projectForm.tsx
+++ components/form/projectForm.tsx
@@ -4,210 +4,240 @@ import AutocompleteInput from "../input/autocomplete";
 import TextArea from "../input/textArea";
 import ComboBox from "../input/comboBox";
 import TextButton from "../button/textButton";
-import Breadcrumb from "@/components/breadcrumb";
-import { toast } from 'react-hot-toast';
+import { toast } from "react-hot-toast";
 import Project, { State } from "@/types/project";
-import { useRouter } from "next/router";
-import projectsList from "@/components/projectsMock";
 
 const stateOptions: State[] = [
-    "OPEN",
-    "CLOSED",
-    "PROGRESS",
-    "BLOCKED",
-    "FINISHED"
+  "OPEN",
+  "CLOSED",
+  "PROGRESS",
+  "BLOCKED",
+  "FINISHED",
 ];
 
 export interface ProjectInputs {
-    [key: string]: boolean
-    name: boolean
-    description: boolean
-    responsable: boolean
-    state: boolean
-    endDateTime: boolean
+  [key: string]: boolean;
+  name: boolean;
+  description: boolean;
+  responsable: boolean;
+  state: boolean;
+  endDateTime: boolean;
 }
 
 export interface ProjectFormProps {
-    project: Project
-    setProject: (project: Project) => void
-    requiredInputs: ProjectInputs
-    disabledInputs: ProjectInputs
-    resources: string[]
-    onSubmit: () => void
-    onCancel: () => void
-    submitButtonName: string
+  project: Project;
+  setProject: (project: Project) => void;
+  requiredInputs: ProjectInputs;
+  disabledInputs: ProjectInputs;
+  resources: string[];
+  onSubmit: () => void;
+  onCancel: () => void;
+  submitButtonName: string;
 }
 
 export default function ProjectForm({
-    project,
-    setProject,
-    onSubmit,
-    onCancel,
-    resources,
-    requiredInputs,
-    disabledInputs,
-    submitButtonName
+  project,
+  setProject,
+  onSubmit,
+  onCancel,
+  resources,
+  requiredInputs,
+  disabledInputs,
+  submitButtonName,
 }: ProjectFormProps) {
-    const [invalidInputs, setInvalidInputs] = useState<ProjectInputs>({
-        name: false,
-        description: false,
-        responsable: false,
-        state: false,
-        endDateTime: false,
-    });
+  const [invalidInputs, setInvalidInputs] = useState<ProjectInputs>({
+    name: false,
+    description: false,
+    responsable: false,
+    state: false,
+    endDateTime: false,
+  });
 
-    const setName = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setProject({ ...project, name: event.target.value });
+  const setName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setProject({ ...project, name: event.target.value });
+  };
+
+  const handleNameFocus = () => {
+    setInvalidInputs({ ...invalidInputs, name: false });
+  };
+
+  const setResponsable = (responsable: string) => {
+    setProject({ ...project, responsable: responsable });
+  };
+
+  const handleResponsableFocus = () => {
+    setInvalidInputs({ ...invalidInputs, responsable: false });
+  };
+
+  const setDescription = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setProject({ ...project, descripcion: event.target.value });
+  };
+
+  const handleDescriptionFocus = () => {
+    setInvalidInputs({ ...invalidInputs, description: false });
+  };
+
+  const setState = (state: string) => {
+    setProject({ ...project, estado: state as State });
+  };
+
+  const setEndDateTime = (endDateTime: string) => {
+    setProject({ ...project, fechaFinalizacion: endDateTime });
+  };
+
+  const handleEndDateTimeFocus = () => {
+    setInvalidInputs({ ...invalidInputs, endDateTime: false });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const finalInvalidInputs: ProjectInputs = {
+      name: requiredInputs.name && project.name.trim() === "",
+      responsable:
+        !disabledInputs.responsable &&
+        requiredInputs.responsable &&
+        !resources.includes(project.responsable),
+      description:
+        requiredInputs.description && project.descripcion.trim() === "",
+      state: requiredInputs.state && !stateOptions.includes(project.estado),
+      endDateTime:
+        !disabledInputs.endDateTime &&
+        requiredInputs.endDateTime &&
+        (project.fechaFinalizacion?.trim() || "") === "",
+    };
+
+    if (!finalInvalidInputs.name) {
+      onSubmit();
+      console.log("aprete el boton de guardar cambios lol");
+    } else {
+      setInvalidInputs(finalInvalidInputs);
+      toast.error("Existen campos incompletos");
     }
+  };
 
-    const handleNameFocus = () => {
-        setInvalidInputs({ ...invalidInputs, name: false });
-    }
-
-    const setResponsable = (responsable: string) => {
-        setProject({ ...project, responsable: responsable });
-    }
-
-    const handleResponsableFocus = () => {
-        setInvalidInputs({ ...invalidInputs, responsable: false });
-    }
-
-    const setDescription = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setProject({ ...project, descripcion: event.target.value });
-    }
-
-    const handleDescriptionFocus = () => {
-        setInvalidInputs({ ...invalidInputs, description: false });
-    }
-
-    const setState = (state: string) => {
-        setProject({ ...project, estado: state as State });
-    }
-
-    const setEndDateTime = (endDateTime: string) => {
-        setProject({ ...project, fechaFinalizacion: endDateTime });
-    }
-
-    const handleEndDateTimeFocus = () => {
-        setInvalidInputs({ ...invalidInputs, endDateTime: false });
-    }
-
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-
-        const finalInvalidInputs: ProjectInputs = {
-            name: requiredInputs.name && project.name.trim() === "",
-            responsable: !disabledInputs.responsable && requiredInputs.responsable && !resources.includes(project.responsable),
-            description: requiredInputs.description && project.descripcion.trim() === "",
-            state: requiredInputs.state && !stateOptions.includes(project.estado),
-            endDateTime: !disabledInputs.endDateTime && requiredInputs.endDateTime && (project.fechaFinalizacion?.trim() || '') === "",
-        };
-        
-        if (!finalInvalidInputs.name) {
-            onSubmit();
-            console.log("aprete el boton de guardar cambios lol")
-        } else {
-            setInvalidInputs(finalInvalidInputs);
-            toast.error("Existen campos incompletos");
-        }
-    }
-
-    return (
-        <form onSubmit={handleSubmit} className="space-y-4">
-            <main className="grid gap-x-6 gap-y-4 sm:grid-cols-1 md:grid-cols-2 border bg-backgroundContainer border-border shadow-lg px-4 py-8 rounded-xl">
-                <Input
-                    title="Nombre"
-                    placeholder="Ingrese un nombre de proyecto"
-                    value={project.name}
-                    setValue={setName}
-                    error={invalidInputs.name}
-                    handleFocus={handleNameFocus}
-                    isRequired={requiredInputs.name}
-                    disabled={disabledInputs.name} />
-                <AutocompleteInput
-                    title="Responsable"
-                    placeholder="Buscar responsable"
-                    value={project.responsable}
-                    setValue={setResponsable}
-                    error={invalidInputs.responsable}
-                    errorText="El responsable debe ser válido"
-                    handleFocus={handleResponsableFocus}
-                    isRequired={requiredInputs.responsable}
-                    items={resources}
-                    disabled={disabledInputs.responsable} />
-                <div className="col-span-full">
-                    <TextArea
-                        title="Descripción"
-                        value={project.descripcion}
-                        setValue={setDescription}
-                        placeholder="Ingrese una descripción"
-                        isRequired={requiredInputs.description}
-                        error={invalidInputs.description}
-                        handleFocus={handleDescriptionFocus}
-                        disabled={disabledInputs.description} />
-                </div>
-                <div className="grid sm:grid-cols-1 md:grid-cols-4 gap-x-6 gap-y-6">
-    <ComboBox
-        title="Estado"
-        options={stateOptions}
-        selected={project.estado}
-        disabled={disabledInputs.state}
-        onChange={setState}
-    />
-    <div className="space-y-2">
-        <label htmlFor="startDate" className="block text-sm font-medium text-title">Fecha de inicio</label>
-        <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+  return (
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <main className="grid gap-x-6 gap-y-4 sm:grid-cols-1 md:grid-cols-2 border bg-backgroundContainer border-border shadow-lg px-4 py-8 rounded-xl">
+        <Input
+          title="Nombre"
+          placeholder="Ingrese un nombre de proyecto"
+          value={project.name}
+          setValue={setName}
+          error={invalidInputs.name}
+          handleFocus={handleNameFocus}
+          isRequired={requiredInputs.name}
+          disabled={disabledInputs.name}
+        />
+        <AutocompleteInput
+          title="Responsable"
+          placeholder="Buscar responsable"
+          value={project.responsable}
+          setValue={setResponsable}
+          error={invalidInputs.responsable}
+          errorText="El responsable debe ser válido"
+          handleFocus={handleResponsableFocus}
+          isRequired={requiredInputs.responsable}
+          items={resources}
+          disabled={disabledInputs.responsable}
+        />
+        <div className="col-span-full">
+          <TextArea
+            title="Descripción"
+            value={project.descripcion}
+            setValue={setDescription}
+            placeholder="Ingrese una descripción"
+            isRequired={requiredInputs.description}
+            error={invalidInputs.description}
+            handleFocus={handleDescriptionFocus}
+            disabled={disabledInputs.description}
+          />
+        </div>
+        <div className="grid sm:grid-cols-1 md:grid-cols-4 gap-x-6 gap-y-6">
+          <ComboBox
+            title="Estado"
+            options={stateOptions}
+            selected={project.estado}
+            disabled={disabledInputs.state}
+            onChange={setState}
+          />
+          <div className="space-y-2">
+            <label
+              htmlFor="startDate"
+              className="block text-sm font-medium text-title"
+            >
+              Fecha de inicio
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                 </svg>
-            </div>
-            <input
+              </div>
+              <input
                 id="startDate"
                 type="text"
                 className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
                 placeholder="Seleccionar fecha"
-                value={project.fechaInicio || ''}
-                onChange={(e) => setProject({ ...project, fechaInicio: e.target.value })}
+                value={project.fechaInicio || ""}
+                onChange={(e) =>
+                  setProject({ ...project, fechaInicio: e.target.value })
+                }
                 onFocus={handleEndDateTimeFocus}
-            />
-        </div>
-    </div>
-    <div className="space-y-2">
-        <label htmlFor="endDate" className="block text-sm font-medium text-title">Fecha de fin</label>
-        <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
+                disabled={disabledInputs.startDate}
+              />
             </div>
-            <input
+          </div>
+          <div className="space-y-2">
+            <label
+              htmlFor="endDate"
+              className="block text-sm font-medium text-title"
+            >
+              Fecha de fin
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                </svg>
+              </div>
+              <input
                 id="endDate"
                 type="text"
                 className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
                 placeholder="Seleccionar fecha"
-                value={project.fechaFinalizacion || ''}
-                onChange={(e) => setProject({ ...project, fechaFinalizacion: e.target.value })}
+                value={project.fechaFinalizacion || ""}
+                onChange={(e) =>
+                  setProject({ ...project, fechaFinalizacion: e.target.value })
+                }
                 onFocus={handleEndDateTimeFocus}
-            />
+                disabled={disabledInputs.endDate}
+              />
+            </div>
+          </div>
         </div>
-    </div>
-</div>
 
-                <div className="flex col-span-full pt-2">
-                    <div className="flex items-center justify-end gap-x-6 px-4 w-full">
-                        <TextButton
-                            name="Cancelar"
-                            style="transparent"
-                            onClick={onCancel} />
-                        <TextButton
-                            name={submitButtonName}
-                            type="submit"
-                            style="primary" />
-                    </div>
-                </div>
-            </main>
-        </form>
-    );
+        <div className="flex col-span-full pt-2">
+          <div className="flex items-center justify-end gap-x-6 px-4 w-full">
+            <TextButton
+              name="Cancelar"
+              style="transparent"
+              onClick={onCancel}
+            />
+            <TextButton name={submitButtonName} type="submit" style="primary" />
+          </div>
+        </div>
+      </main>
+    </form>
+  );
 }
--- pages/projects/gestionarProyectos/[project]/[projectView].tsx
+++ pages/projects/gestionarProyectos/[project]/[projectView].tsx
@@ -11,150 +11,161 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ManageProject() {
-    const router = useRouter();
-    const { project: projectId } = router.query;
+  const router = useRouter();
+  const { project: projectId } = router.query;
 
-    const disabledInputs: ProjectInputs = {
-        name: true,
-        description: true,
-        responsable: true,
-        state: true,
-        endDate: true,
-        startDate: true
-    }
+  const disabledInputs: ProjectInputs = {
+    name: true,
+    description: true,
+    responsable: true,
+    state: true,
+    endDate: true,
+    startDate: true,
+  };
 
-    const requiredInputs: ProjectInputs = {
-        name: true,
-        description: false,
-        responsable: false,
-        state: false,
-        endDateTime: false
-    }
+  const requiredInputs: ProjectInputs = {
+    name: true,
+    description: false,
+    responsable: false,
+    state: false,
+    endDateTime: false,
+  };
 
-    const [project, setProject] = useState(emptyProject)
+  const [project, setProject] = useState(emptyProject);
 
-
-    
-    useEffect(() => {
-        if (projectId) {
-            fetch("https://projects-backend-am35.onrender.com/projects", {
-                method: "GET",
-                headers: {
-                    "Accept": "*/*"
-                }
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error("Error al obtener la lista de proyectos");
-            })
-            .then(data => {
-                // Mapea los datos obtenidos para que coincidan con el formato esperado
-                const projects = data.map(project => ({
-                    id: project.id,
-                    name: project.title,
-                    responsable: project.assignedLeader || 'Sin asignar', // Ajustar según sea necesario
-                    fechaFinalizacion: project.finishDate ? new Date(project.finishDate).toLocaleDateString() : 'Sin fecha',
-                    fechaInicio: new Date(project.startDate).toLocaleDateString(),
-                    estado: project.state,
-                    descripcion: project.description
-                }));
-
-                const currentProject = projects.find(proj => proj.id === Number(projectId));
-                if (currentProject) {
-                    setProject(currentProject);
-                } else {
-                    console.error("Project not found");
-                }
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-        }
-    }, [projectId]);
-
-
-    const [resources, setResources] = useState<Resource[]>([])
-    useEffect(() => {
-        getResources().then((resources) => setResources(resources)).catch((e) => console.log(e))
-    }, [])
-
-    const onCancel = () => {
-        router.back()
-    }
-
-    const onSubmit = () => {
-        
-        console.log("datos del proyecto");
-        console.log(project.id);
-        console.log(project.name);
-        console.log(project.descripcion);
-        console.log(project.estado);
-        console.log(project.fechaInicio);
-        console.log(project.fechaFinalizacion);
-        console.log(project.responsable);
-    
-        const url = `https://projects-backend-am35.onrender.com/projects/${project.id}?assigned_leader=${project.responsable}&description=${project.descripcion}&state=${project.estado}&title=${project.name}`;
-    
-        console.log("la url es:")
-        console.log(url)
-        fetch(url, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'accept': '*/*'
-            }
+  useEffect(() => {
+    if (projectId) {
+      fetch("https://projects-backend-am35.onrender.com/projects", {
+        method: "GET",
+        headers: {
+          Accept: "*/*",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Error al obtener la lista de proyectos");
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            // Check if response is not empty before parsing
-            return response.text().then(text => {
-                return text ? JSON.parse(text) : {};
-            });
-        })
-        .then(data => {
-            console.log('Success:', data);
-            //toast.success("");
-            router.push(`/projects/gestionarProyectos`);
+        .then((data) => {
+          // Mapea los datos obtenidos para que coincidan con el formato esperado
+          const projects = data.map((project) => ({
+            id: project.id,
+            name: project.title,
+            responsable: project.assignedLeader || "Sin asignar", // Ajustar según sea necesario
+            fechaFinalizacion: project.finishDate
+              ? new Date(project.finishDate).toLocaleDateString()
+              : "Sin fecha",
+            fechaInicio: new Date(project.startDate).toLocaleDateString(),
+            estado: project.state,
+            descripcion: project.description,
+          }));
+
+          const currentProject = projects.find(
+            (proj) => proj.id === Number(projectId)
+          );
+          if (currentProject) {
+            setProject(currentProject);
+          } else {
+            console.error("Project not found");
+          }
         })
         .catch((error) => {
-            console.error('Error:', error);
-            toast.error("Hubo un error al guardar los cambios");
+          console.error("Error:", error);
         });
-    };
-    
+    }
+  }, [projectId]);
 
-    return (
-        <div>
-            <Breadcrumb steps={[
-                { name: "Proyectos", link: "/projects/" },
-                { name: "Gestión de Proyectos", link: "/projects/gestionarProyectos" },
-                { name: `${projectId}`, link: null }
-            ]} />
-            <div className="space-y-4">
-                <header className="flex items-center">
-                  <h1 className="text-2xl sm:text-4xl font-bold text-title line-clamp-2 sm:line-clamp-1 py-1">Gestionar Proyecto</h1>
-                </header>
-                <ProjectForm
-                    project={project}
-                    setProject={setProject}
-                    disabledInputs={disabledInputs}
-                    requiredInputs={requiredInputs}
-                    submitButtonName={'Aceptar'}
-                    //onSubmit={onSubmit}
-                    onCancel={onCancel}
-                    resources={resources.map((resource) => resource.Nombre + " " + resource.Apellido)}
-                    />
-                <div className="flex items-center justify-start gap-x-6 px-4">
-                    <TextButton
-                        name="Volver"
-                        style="transparent"
-                        onClick={() => router.back()} />
-                </div>
-            </div>
+  const [resources, setResources] = useState<Resource[]>([]);
+  useEffect(() => {
+    getResources()
+      .then((resources) => setResources(resources))
+      .catch((e) => console.log(e));
+  }, []);
+
+  const onCancel = () => {
+    router.back();
+  };
+
+  const onSubmit = () => {
+    console.log("datos del proyecto");
+    console.log(project.id);
+    console.log(project.name);
+    console.log(project.descripcion);
+    console.log(project.estado);
+    console.log(project.fechaInicio);
+    console.log(project.fechaFinalizacion);
+    console.log(project.responsable);
+
+    const url = `https://projects-backend-am35.onrender.com/projects/${project.id}?assigned_leader=${project.responsable}&description=${project.descripcion}&state=${project.estado}&title=${project.name}`;
+
+    console.log("la url es:");
+    console.log(url);
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "*/*",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        // Check if response is not empty before parsing
+        return response.text().then((text) => {
+          return text ? JSON.parse(text) : {};
+        });
+      })
+      .then((data) => {
+        console.log("Success:", data);
+        //toast.success("");
+        router.push(`/projects/gestionarProyectos`);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("Hubo un error al guardar los cambios");
+      });
+  };
+
+  return (
+    <div>
+      <Breadcrumb
+        steps={[
+          { name: "Proyectos", link: "/projects/" },
+          {
+            name: "Gestión de Proyectos",
+            link: "/projects/gestionarProyectos",
+          },
+          { name: `${projectId}`, link: null },
+        ]}
+      />
+      <div className="space-y-4">
+        <header className="flex items-center">
+          <h1 className="text-2xl sm:text-4xl font-bold text-title line-clamp-2 sm:line-clamp-1 py-1">
+            Gestionar Proyecto
+          </h1>
+        </header>
+        <ProjectForm
+          project={project}
+          setProject={setProject}
+          disabledInputs={disabledInputs}
+          requiredInputs={requiredInputs}
+          submitButtonName={"Aceptar"}
+          onSubmit={onSubmit}
+          onCancel={onCancel}
+          resources={resources.map(
+            (resource) => resource.Nombre + " " + resource.Apellido
+          )}
+        />
+        <div className="flex items-center justify-start gap-x-6 px-4">
+          <TextButton
+            name="Volver"
+            style="transparent"
+            onClick={() => router.back()}
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
